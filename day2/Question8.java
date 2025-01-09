fefc06d480717686924b0495d49449d5930cd050
Jerry2732002 <jerrysebastian2732002@gmail.com>
2025-01-09 18:30:11 +0530

    day 2 DSA commit

--- day2/Question8.java
+++ day2/Question8.java
@@ -1,4 +1,22 @@
 package day2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Question8 {
+    public static int fruitPick(String[] arr) {
+        Map<String, Integer> fruit = new HashMap<>();
+        for (String str : arr) {
+            if (fruit.containsKey(str)) {
+                fruit.put(str, fruit.get(str) + 1);
+            } else {
+                fruit.put(str,0);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
