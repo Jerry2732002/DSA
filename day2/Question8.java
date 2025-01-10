0ed5be0d1b1c2cbe21cd9ff263cbe60bf04326c5
Jerry2732002 <jerrysebastian2732002@gmail.com>
2025-01-10 09:27:03 +0530

    day 2 DSA commit

--- day2/Question8.java
+++ day2/Question8.java
@@ -1,22 +1,40 @@
 package day2;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Question8 {
     public static int fruitPick(String[] arr) {
+        int result = 0;
         Map<String, Integer> fruit = new HashMap<>();
         for (String str : arr) {
             if (fruit.containsKey(str)) {
                 fruit.put(str, fruit.get(str) + 1);
             } else {
-                fruit.put(str,0);
+                fruit.put(str, 1);
             }
         }
+        Map.Entry<String, Integer> entry = fruit.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .findFirst()
+                .get();
 
+        result += entry.getValue();
+
+        entry = fruit.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .skip(1)
+                .findFirst()
+                .get();
+        result += entry.getValue();
+
+
+        return result;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(fruitPick(new String[]{"A","B","A","B","A","B"}));
     }
 }
